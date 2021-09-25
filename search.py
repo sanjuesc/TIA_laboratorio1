7ecfa9e52a9b79591e658809d8f04f6706b45bd6
Ander <asanjuan017@ikasle.ehu.eus>
2021-09-25 17:51:13 +0200

    el laberinto lo hace finisimo, pero el autograder no
    me quiero cortar la polla, cortarme la polla, ponerla en un plato y comermela

--- search.py
+++ search.py
@@ -85,9 +85,49 @@ def depthFirstSearch(problem):
     print("Start:", problem.getStartState())
     print("Is the start a goal?", problem.isGoalState(problem.getStartState()))
     print("Start's successors:", problem.getSuccessors(problem.getStartState()))
+
+
+    https://www.hackerearth.com/practice/algorithms/graphs/depth-first-search/tutorial/
+          let S be stack
+      S.push( s )            //Inserting s in stack
+      mark s as visited.
+      while ( S is not empty):
+          //Pop a vertex from stack to visit next
+          v  =  S.top( )
+         S.pop( )
+         //Push all the neighbours of v in stack that are not visited
+        for all neighbours w of v in Graph G:
+            if w is not visited :
+                     S.push( w )
+                    mark w as visited
+
     """
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+
+    caminosPosibles = util.Stack()
+    visitados = []
+    caminosPosibles.push((problem.getStartState(), [])) #Estamos en la posicion inicial y hemos hecho [] movimientos en este camino
+    visitados.append(problem.getStartState()[0]) #no es necesario realmente, pero lo añadimos a visitados por que si no volveriamos a el en cada uno de sus succesors para comprobar si es el final
+                                              # luego mirariamos su succesors de nuevo, los cuales estarian ya visitados y bla bla bla... en resumen: perderiamos tiempo
+
+    while not caminosPosibles.isEmpty():
+
+        elementoActual,lista_movimientos = caminosPosibles.pop() #Cogemos el elemento y los movimientos hechos para llegar a el
+        for successor in problem.getSuccessors(elementoActual):
+            pos=successor[0]
+            dir=successor[1]
+            if pos not in visitados: #Si ya hemos visitado el elemento pasamos de el, por que ya estara en el camino que hemos recorrido
+                if(problem.isGoalState(pos)):
+                    actualizada = lista_movimientos + [dir] #podemos ahorrarnos estas dos lineas y returnear directamene el append
+                    #print(actualizada)
+                    return(actualizada)
+                else:
+                    actualizada = lista_movimientos + [dir] #podemos ahorrarnos estas dos lineas y returnear directamene el append
+                    #print(actualizada)
+                    caminosPosibles.push((pos, actualizada)) #lo haria con arrays de numpy pero el coste de hacer un append en python es O(1) asi que solo haria el programa mas lento
+                    #Dejo el codigo de todas formas por si en el futuro necesitamos usar arrays de numpy para optimizar otros ejercicios
+                    ##caminosPosibles.push((pos, np.append(np.array(lista_movimientos),np.array(dir).tolist())))
+                    visitados.append(pos)
 
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
